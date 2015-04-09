a0d87f7f07e3ed2abe31938739fc74c89b8d92a5
Jean Marc BRUNO <technique@edps.fr>
2015-04-09 17:26:58 +0200

    Corection des warnings Java

--- src/ToForeground.java
+++ src/ToForeground.java
@@ -21,35 +21,25 @@ package com.diusrex.toforeground;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
-import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
 import org.json.JSONException;
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.ComponentName;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.net.Uri;
 
 public class ToForeground extends CordovaPlugin {
 
     public boolean execute(String className, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        try {
-          Intent it = new Intent("android.intent.action.MAIN");
 
-          String packageName = args.getString(0);
+        Intent it = new Intent("android.intent.action.MAIN");
 
-          Activity activity = this.cordova.getActivity();
-          it.setComponent(new ComponentName(packageName, packageName + "." + className));
-	  it.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-          
-          Context context = activity.getApplicationContext();
-          context.startActivity(it);
-        }
-        catch (Exception e) {
-          return false;
-        }
+        String packageName = args.getString(0);
+        it.setComponent(new ComponentName(packageName, packageName + "." + className));
+	it.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+        Context context = this.cordova.getActivity().getApplicationContext();
+        context.startActivity(it);
         return true;
     }
 };
